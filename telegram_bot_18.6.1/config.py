86baa9d1f8a2f4a8cb395867c6a94798a93b04d9
AndreiSon88 <avm.tehniks@yandex.ru>
2022-05-24 15:35:38 +0300

    Signed-off-by: AndreiSon88 <avm.tehniks@yandex.ru>

--- telegram_bot_18.6.1/config.py
+++ telegram_bot_18.6.1/config.py
@@ -1,6 +1,3 @@
-# Your API Key: 2c7161729c4d19471144f5a8
-# Example Request: https://v6.exchangerate-api.com/v6/2c7161729c4d19471144f5a8/latest/USD
-# https://v6.exchangerate-api.com/v6/2c7161729c4d19471144f5a8/pair/USD/EUR
 
 TOKEN = '5314601789:AAFMLWX4b1DMZkZO3-gQ1vndVIeWT0b5Grw'
 keys = {
--- telegram_bot_18.6.1/extensions.py
+++ telegram_bot_18.6.1/extensions.py
@@ -24,5 +24,9 @@ class MoneyConverter:
         except ValueError:
             raise ConvertionException(f'Не удалось обработать колличество {amount}')
         r = requests.get(f'https://v6.exchangerate-api.com/v6/2c7161729c4d19471144f5a8/pair/{quote_ticker}/{base_ticker}')
+        # Your API Key: 2c7161729c4d19471144f5a8
+        # Example Request: https://v6.exchangerate-api.com/v6/2c7161729c4d19471144f5a8/pair/USD/EUR
+
+
         total_base = json.loads(r.content)['conversion_rate']
         return round(total_base,2)*amount
